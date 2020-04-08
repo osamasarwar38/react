507cb493e180c6bf23b14f8dd9e566459fcbdc4a
osamasarwar38 <osamasarwar38@gmail.com>
2020-04-09 04:46:21 +0500

    Class Quiz 5 Task 2

---
+++ Osama.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Osama extends React.Component {
+    
+   
+    render() {
+
+        const mystyle = {
+
+            
+          };
+
+    return (
+    <div style={{ color:"#59c8e6" }}>
+    <h1>Osama Sarwar</h1>
+    <img src={'https://frontendmasters.com/static-assets/learn/og-learning-path-react.jpg'} height={300} width={600} />
+    </div>
+    ); }
+    }
+  
+  export default Osama;
